dae6f00fa6f5be84657560b2c9b1f8b5e22fb9e3
Gonglab-THU <gonglab_thu@hotmail.com>
2025-06-04 13:21:24 +0800

    fix typo

--- train_code/SPIRED-Fitness/train_stage1.sh
+++ train_code/SPIRED-Fitness/train_stage1.sh
@@ -2,5 +2,5 @@ echo start `date`
 python scripts_stage1/run.py \
         --train_valid_list data/stage1/fitness_samples_train_valid.csv \
         --training_data data/stage1/mutation_data.pt \
-        --outdir output_stage1 \ 
+        --outdir output_stage1 
 echo end `date`
